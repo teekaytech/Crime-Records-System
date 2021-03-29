0aee9e632c5c495ad93d60e905d73ea339297013
teekaytech <taofeek.olalere@feghas.com>
2021-03-29 18:23:07 +0100

    Feat: Dashboard Design
    Create project dashboard. Add nav and card components

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
 import "../stylesheets/application"
+import "@fortawesome/fontawesome-free/css/all";
+
 
 Rails.start()
 Turbolinks.start()
